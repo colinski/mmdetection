2a5d9dda97a96de2760f45bfbf07d48ea7580fec
Colin <csamplawski@cs.umass.edu>
2022-04-10 01:11:39 -0400

    added missing dir in exps/

--- exps/detr_r50_8x2_200e_coco/train.sh
+++ exps/detr_r50_8x2_200e_coco/train.sh
@@ -7,6 +7,6 @@ singularity run --nv sif/python.sif bash \
     src/mmdetection/configs/detr/detr_r50_8x2_200e_coco.py \
     8 \
     --seed 1 \
-    --work-dir logs/detr_r50_8x2_200e_coco
+    --work-dir src/mmdetection/exps/detr_r50_8x2_200e_coco
 
 cd $START
